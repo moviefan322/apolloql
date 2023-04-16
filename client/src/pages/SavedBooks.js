dedeb2f28ef0b507299557eecb46b939a1ecd210
moviefan322 <philman202@yahoo.com>
2023-04-15 21:26:57 -0400

    removes unused useEffect requirement from SavedBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
